8269e2608ada5b0b2e8f3cb08d05d7c2169c81f5
Svetlana Ponomareva <svetlana.andreyevna@gmail.com>
2022-10-18 01:11:58 +0300

    hm

--- Seminar_2/HM_2Sem/task2_1.py
+++ Seminar_2/HM_2Sem/task2_1.py
@@ -5,3 +5,28 @@
 
 # - 6782 -> 23
 # - 0,56 -> 11
+
+# через строку:
+
+# n = input('Введите число: ')
+# sum = 0
+
+# for digit in n:
+#     if digit.isdigit():
+#         sum += int(digit)
+
+
+# print(f'Сумма цифр = {sum}')
+
+
+# для целого числа:
+
+def sum_of_digits(num):
+    sum = 0
+    while num > 0:
+        sum += num % 10
+        num //= 10
+    return sum
+
+print(sum_of_digits(66)) 
+
--- Seminar_2/HM_2Sem/task2_4.py
+++ Seminar_2/HM_2Sem/task2_4.py
@@ -1,2 +1,16 @@
 # Задача 4 НЕОБЯЗАТЕЛЬНАЯ. Напишите программу, которая принимает на вход N, и координаты двух точек и находит расстояние между ними в N-мерном пространстве.
 
+Доделать!
+
+import numpy as np
+
+N = int(input("Введите число N: "))
+
+vector1 = int(input(np.array([])))
+vector2 = int(input(np.array([])))
+ 
+op1=np.sqrt(np.sum(np.square(vector1-vector2)))
+print(op1)
+
+
+
